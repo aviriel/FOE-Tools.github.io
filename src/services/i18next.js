f21bb071ddfa84d79adc3c3ff4f93b4fefcadcbd
Michael Hudak <email@michael-hudak.de>
2018-02-05 12:21:05 +0100

    feature/i18n-de: adding language german
    translation completed

--- src/services/i18next.js
+++ src/services/i18next.js
@@ -3,20 +3,21 @@ import Cookies from "js-cookie";
 
 import en from "~/i18n/en.json";
 import fr from "~/i18n/fr.json";
+import de from "~/i18n/de.json";
 
 if (Cookies.get("lang") === undefined) {
   Cookies.set("lang", "en");
 }
 
-const whitelist = ["en", "fr"];
-const resources = { en: en, fr: fr };
+const whitelist = ["en", "fr", "de"];
+const resources = { en: en, fr: fr, de: de };
 
 i18next.init(
   {
     lng: Cookies.get("lang"),
     debug: true,
     whitelist: whitelist,
-    fallbackLng: ["en", "fr"],
+    fallbackLng: ["en", "fr", "de"],
     resources: resources
   },
   function() {}
